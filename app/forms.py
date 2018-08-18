4ea263d790b941684355899a7914c3720020c0b6
yong lu <yong.lu@zuolin.com>
2018-08-18 23:41:33 +0800

    finish cp4

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
-from flask_wtf import flaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 
-class LoginForm(flaskForm):
+class LoginForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
