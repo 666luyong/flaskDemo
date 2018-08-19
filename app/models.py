f8fbc17569a0b72d969ccc79567ccf5bc5c0a795
yong lu <yong.lu@zuolin.com>
2018-08-19 19:50:23 +0800

    cp 4 done

--- app/models.py
+++ app/models.py
@@ -1,11 +1,13 @@
 from app import db
 from datetime import datetime
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    post = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -16,5 +18,6 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     body = db.Column(db.String(140))
+
     def __repr__(self):
         return '<Post {}>'.format(self.body)
