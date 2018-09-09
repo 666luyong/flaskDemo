f298faab739fd3453797cf847b3805ca4f9a2fd4
yong lu <yong.lu@zuolin.com>
2018-09-09 18:36:30 +0800

    mid cp 5

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,30 @@
 from app import app
-from flask import render_template,flash,redirect,url_for
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user
 
-@app.route('/login', methods = ['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        print('will flash')
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        loginName = form.username.data
+        print("loginName :" + loginName)
+        user = User.query.all()
+        user
+        print("user  count :")
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
-    return render_template('login.html',title='Sign In', form = form)
+    return render_template('login.html', title='Sign In', form=form)
+
 
 @app.route('/')
 @app.route('/index')
@@ -28,3 +43,9 @@ def index():
         'body': 'The Avengers movie was so cool'
     }]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
