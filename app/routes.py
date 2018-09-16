5335024918de09d9d7843035e353dc98c3a7cb80
yong.lu <yong.lu@zuolin.com>
2018-09-16 12:57:28 +0800

    add debug sh

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ from flask_login import login_required
 from werkzeug.urls import url_parse
 from app.forms import RegistrationForm
 from app import db
-
+from datetime import datetime
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -80,3 +80,9 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
